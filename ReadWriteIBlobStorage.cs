240fc008875782b6d3e09b52c8dc80f442b8ed19
PankajAaarav <107175163+PankajAaarav@users.noreply.github.com>
2022-06-09 13:07:13 +0530

    Add files via upload

---
+++ ReadWriteIBlobStorage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureWebAPI
+{
+    public class ReadWriteIBlobStorage
+    {
+        public void ReadFile()
+        {
+            string storageConnection = "";
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("containerName");
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference("fileName");
+            cloudBlockBlob.DownloadTextAsync();
+
+        } 
+        public void WriteFile()
+        {
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("");
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("containerName");
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference("blobName");
+            cloudBlockBlob.UploadTextAsync("This is for testing");
+        }
+    }
+}
---
+++ ReadWriteInQueue.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureWebAPI
+{
+    public class ReadWriteInQueue
+    {
+        public void WriteInQueue()
+        {
+            string connectionString = "Your Azure Storage Connection String";
+            string queueName = "test";
+            QueueClient queue = new QueueClient(connectionString, queueName);
+            queue.Create();
+            queue.SendMessage("This is the first message.");
+            queue.SendMessage("This is the second message.");
+            foreach (QueueMessage message in queue.ReceiveMessages(maxMessages: 100).Value)
+            {
+                //Write your code here to process the messages
+                queue.DeleteMessage(message.MessageId, message.PopReceipt);
+                //Delete the message once it has been processed
+            }
+        }
+        public static bool DeleteQueue(string queueName)
+        {
+            try
+            {
+                string connectionString = "Your Azure Storage Connection String";
+                QueueClient queueClient = new QueueClient
+                (connectionString, queueName);
+                if (queueClient.Exists())
+                {
+                    queueClient.Delete();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+}
